8f366878a053675a56ec85857430083ea179d049
Amit Khayargoli <86340673+amitKhayargoli@users.noreply.github.com>
2025-02-25 20:55:24 +0545

    image uploads

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -5,15 +5,17 @@ import {
   deleteUser,
   getAllUsers,
   getUserById,
+  getUsersbyTeam,
 } from "../controllers/userController.js";
 const router = express.Router();
 
 // Define routes
 router.post("/", create);
 
-router.put("/:id", update);
+router.put("/", update);
 router.delete("/:id", deleteUser);
 router.get("/", getAllUsers);
+router.get("/team", getUsersbyTeam);
 router.get("/:id", getUserById);
 
 export default router;
