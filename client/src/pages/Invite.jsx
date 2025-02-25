cf66079a30528f279d0666f6a5f646de2e927eb0
Amit Khayargoli <86340673+amitKhayargoli@users.noreply.github.com>
2025-02-25 21:05:33 +0545

    invite page

--- client/src/pages/Invite.jsx
+++ client/src/pages/Invite.jsx
@@ -1,20 +1,73 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-import HomeNav from "../components/HomeNav";
-import invite from "../images/invite.png";
-const Invite = () => {
-  return (
-    <>
-      <HomeNav />
-      <div className="bg-black w-full h-screen px-20 flex flex-col items-center">
-        <h1 className="text-6xl font-bold text-white text-center mt-8 mb-8">
-          Invitation
-        </h1>
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-        <img className="rounded-md h-[600px]" src={invite} alt="" />
+import invite from "../images/invite2.png";
+import HomeNav from "../components/HomeNav";
+import { toast } from "react-toastify";
+const JoinProject = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const token = queryParams.get("token");
+
+    if (token) {
+      localStorage.setItem("InviteToken", token);
+
+      const userToken = localStorage.getItem("token");
+
+      if (!userToken) {
+        navigate("/login");
+      } else {
+        joinProject(token, userToken);
+      }
+    } else {
+      setError("No token found in the URL.");
+    }
+  }, [location]);
+
+  const joinProject = async (inviteToken, authToken) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/invite?token=${inviteToken}`,
+        {
+          headers: { Authorization: `Bearer ${authToken}` },
+        }
+      );
+
+      setMessage(response.data.message);
+      localStorage.setItem("joinedProject", "true");
+      localStorage.removeItem("InviteToken");
+
+      setTimeout(() => {
+        navigate("/workspace/projects");
+      }, 2000);
+    } catch (error) {
+      const errorMessage = error.response?.data?.error || "An error occurred";
+
+      if (errorMessage === "User already a member of the project") {
+        navigate("/workspace/projects");
+      } else {
+        setError(errorMessage);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <HomeNav />
+      <div className="mt-8 px-30">
+        <h1 className="text-2xl md:text-6xl font-bold text-white text-center mb-10">
+          Accepting Invitation
+        </h1>
+        <img src={invite} alt="" />
       </div>
-    </>
+    </div>
   );
 };
 
-export default Invite;
+export default JoinProject;
