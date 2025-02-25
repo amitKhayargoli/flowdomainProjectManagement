018cf5e665e5e65e9d0b691a05213985351c0aed
Amit Khayargoli <86340673+amitKhayargoli@users.noreply.github.com>
2025-02-25 21:03:51 +0545

    added joinproject message

--- client/src/components/Workspace.jsx
+++ client/src/components/Workspace.jsx
@@ -1,5 +1,5 @@
 // src/DashboardLayout.js
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleSidebar, toggleDarkMode } from "../redux/globalSlice";
 import Navbar from "./Navbar";
@@ -8,8 +8,10 @@ import "@fontsource/inter/400.css";
 
 import "../index.css";
 import { Outlet } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Workspace = () => {
+  const [joinedMessage, setJoinedMessage] = useState("");
   const dispatch = useDispatch();
   const isSidebarCollapsed = useSelector(
     (state) => state.global.isSidebarCollapsed
@@ -17,6 +19,12 @@ const Workspace = () => {
   const isDarkMode = useSelector((state) => state.global.isDarkMode);
 
   useEffect(() => {
+    if (localStorage.getItem("joinedProject")) {
+      setJoinedMessage("Successfully joined project!");
+      localStorage.removeItem("joinedProject");
+      toast.success(joinedMessage);
+    }
+
     if (isDarkMode) {
       document.documentElement.classList.add("dark");
     } else {
